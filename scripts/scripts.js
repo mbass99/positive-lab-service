2ea2a64c2230f0c23b21237760428a740ca003e7
Matt Bass <mattbass99@gmail.com>
2020-10-02 14:21:46 -0700

    js page adding missing $ and space .slick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,21 @@
 // SLIDER JAVASCRIPT
 
 $(document).ready(() => {
-      $('.slider .hero-sliders').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        slidestoScroll: 3,
-        slidesToShow: 1,
-      });
+  $(".slider .hero-sliders").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    slidestoScroll: 3,
+    slidesToShow: 1,
+  });
+});
 
-
-      });
-
-      $(document).ready(() => {
-        ('.resources-slider-div .resources-sliders') .slick({
-          autoplay: true,
-          autoplaySpeed: 3000,
-          dots: true,
-          slidestoScroll: 4,
-          slidesToShow: 3,
-        });
-  
-  
-        });
-
-
-
-
-
-
-     
+$(document).ready(() => {
+  $(".resources-slider-div .resources-sliders").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    slidestoScroll: 4,
+    slidesToShow: 3,
+  });
+});
