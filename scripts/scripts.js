5e47031b09d23cdaec28bcc9b0003027e9f320bc
Matt Bass <mattbass99@gmail.com>
2020-10-02 14:12:47 -0700

    fixing services jquery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,32 @@
 // SLIDER JAVASCRIPT
 
 $(document).ready(() => {
-  $('.slider .hero-sliders').slick({
-    autoplay: true,
-    autoplaySpeed: 5000,
-    dots: true,
-  });
+      $('.slider .hero-sliders').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        slidestoScroll: 3,
+        slidesToShow: 1,
+      });
 
-});
+
+      });
+
+      $(document).ready(() => {
+        ('.resources-slider-div .resources-sliders') .slick({
+          autoplay: true,
+          autoplaySpeed: 3000,
+          dots: true,
+          slidestoScroll: 4,
+          slidesToShow: 3,
+        });
+  
+  
+        });
+
+
+
+
+
+
+     
