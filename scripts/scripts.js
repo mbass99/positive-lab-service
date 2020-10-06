a79bfbf6562fd2015579d409d60bc4fcd8b619db
Matt Bass <mattbass99@gmail.com>
2020-10-05 23:23:06 -0700

    javascript changes in slick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,12 +10,38 @@ $(document).ready(() => {
   });
 });
 
-$(document).ready(() => {
-  $(".resources-slider-div .resources-sliders").slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
-    slidestoScroll: 4,
-    slidesToShow: 3,
-  });
+$('.resources-sliders').slick({
+  autoplay: true,
+  dots: true,
+  infinite: false,
+  speed: 3000,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
